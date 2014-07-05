022e9796438a4a0c8c5c4ecfab07cbad1ce411e4
Vinícius Rodrigues dos Santos <vinicius-rs@hotmail.com.br>
2014-07-05 11:23:40 -0300

    Sistema de login implementado (suporte MD5)

--- Caronada/LoginADM.Designer.cs
+++ Caronada/LoginADM.Designer.cs
@@ -29,41 +29,42 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginADM));
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.tbID = new System.Windows.Forms.TextBox();
+            this.tbSW = new System.Windows.Forms.TextBox();
+            this.btnEntrar = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // textBox1
+            // tbID
             // 
-            this.textBox1.Location = new System.Drawing.Point(79, 66);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(159, 20);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.tbID.Location = new System.Drawing.Point(79, 66);
+            this.tbID.Name = "tbID";
+            this.tbID.Size = new System.Drawing.Size(159, 20);
+            this.tbID.TabIndex = 0;
             // 
-            // textBox2
+            // tbSW
             // 
-            this.textBox2.Location = new System.Drawing.Point(79, 92);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.PasswordChar = '-';
-            this.textBox2.Size = new System.Drawing.Size(159, 20);
-            this.textBox2.TabIndex = 1;
+            this.tbSW.Location = new System.Drawing.Point(79, 92);
+            this.tbSW.MaxLength = 32;
+            this.tbSW.Name = "tbSW";
+            this.tbSW.PasswordChar = '-';
+            this.tbSW.Size = new System.Drawing.Size(159, 20);
+            this.tbSW.TabIndex = 1;
+            this.tbSW.WordWrap = false;
             // 
-            // button1
+            // btnEntrar
             // 
-            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
-            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(79, 118);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(84, 48);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Entrar";
-            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnEntrar.Image = ((System.Drawing.Image)(resources.GetObject("btnEntrar.Image")));
+            this.btnEntrar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEntrar.Location = new System.Drawing.Point(79, 118);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(84, 48);
+            this.btnEntrar.TabIndex = 2;
+            this.btnEntrar.Text = "Entrar";
+            this.btnEntrar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnEntrar.UseVisualStyleBackColor = true;
+            this.btnEntrar.Click += new System.EventHandler(this.btnEntrar_Click);
             // 
             // label1
             // 
@@ -90,12 +91,11 @@
             this.ClientSize = new System.Drawing.Size(284, 261);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnEntrar);
+            this.Controls.Add(this.tbSW);
+            this.Controls.Add(this.tbID);
             this.Name = "LoginADM";
             this.Text = "Login ADM";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -103,9 +103,9 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbID;
+        private System.Windows.Forms.TextBox tbSW;
+        private System.Windows.Forms.Button btnEntrar;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
 
--- Caronada/LoginADM.cs
+++ Caronada/LoginADM.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Data.SqlClient;
 
 namespace Caronada
 {
@@ -17,21 +19,71 @@ namespace Caronada
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        public static string dbConString = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\Caronada.mdf;Integrated Security=True;Connect Timeout=30";
+
+        private void btnEntrar_Click(object sender, EventArgs e)
         {
-            Opções op = new Opções();
-            this.Hide();
-            op.Show();
+            if (testarADM())
+            {
+                Opções op = new Opções();
+                this.Hide();
+                op.Show();
+            }
+            else
+            {
+                MessageBox.Show("ERRO DE LOGIN!");
+            }
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private bool testarADM()
         {
+            SqlConnection sqlCon;
+            SqlCommand sqlComm;
+            SqlDataReader dataReader;
+            String id, sw;
+            byte[] hash;
+            MD5 hasher;
+            bool loginMatch = false;
+            String dbID, dbSW;
 
-        }
+            id = tbID.Text;
+            sw = tbSW.Text;
+            hasher = MD5.Create();
+            hash = Encoding.UTF8.GetBytes(sw);
+            hash = hasher.ComputeHash(hash);
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
+            sw = "";
 
+            foreach (byte b in hash)
+            {
+                sw = sw + b.ToString("x2");
+            }
+
+            sqlCon = new SqlConnection(dbConString);
+            sqlComm = new SqlCommand("SELECT * FROM ADMINISTRADOR", sqlCon);
+
+            sqlCon.Open();
+            dataReader = sqlComm.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                dbID = dataReader.GetString(0);
+
+                if (dbID == id)
+                {
+                    dbSW = dataReader.GetString(1);
+
+                    if (dbSW == sw)
+                    {
+                        loginMatch = true;
+                        break;
+                    }
+                }
+            }
+
+            sqlCon.Close();
+
+            return loginMatch;
         }
     }
 }
